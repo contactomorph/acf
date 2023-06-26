c491364fe8e575eba9d53b44dbaf1422d342f4f2
Eddy Cizeron <eddy.cizeron@shift-technology.com>
2023-06-26 11:44:55 +0200

    Add resize logic in RunningBar

--- app/components/RunningBar.tsx
+++ app/components/RunningBar.tsx
@@ -11,14 +11,15 @@ export type Block = {
     readonly texts: ReadonlyArray<string>,
 };
 
-function createBlock(proportion: number, block: Block) : JSX.Element {
+function RunningBlock(props: { block: Block, proportion: number }) : JSX.Element {
+    const block = props.block;
     const texts = block.texts.map(t => (<><br/>{t}</>));
     const color = block.color;
     const backgroundColor = color.desaturate().hex();
     const borderColor = color.darken(2).hex();
     const textColor = color.luminance() > 0.5 ? color.darken(3).hex() : color.brighten(3).hex();
     const style: Properties = {
-        width: `${100 * proportion}%`,
+        width: `${100 * props.proportion}%`,
         borderColor,
         backgroundColor,
         color: textColor,
@@ -29,12 +30,60 @@ function createBlock(proportion: number, block: Block) : JSX.Element {
     </div>);
 }
 
+const MINIMAL_ACCEPTABLE_PX_SIZE = 50;
+
+function mayExtendSequence(
+    inner: HTMLDivElement,
+    seq: HTMLDivElement,
+    minProportion: number
+) {
+    const minWidthIfContracted = inner.clientWidth * minProportion;
+    if (minWidthIfContracted < MINIMAL_ACCEPTABLE_PX_SIZE) {
+        const extendedPercentage = Math.floor(
+            100 * MINIMAL_ACCEPTABLE_PX_SIZE / minWidthIfContracted
+        );
+        inner.style.overflowX = "scroll";
+        seq.style.width = `${extendedPercentage}%`;
+    } else {
+        seq.style.width = "100%";
+        inner.style.overflowX = 'auto';
+    }
+}
+
 export function RunningBar(props: { blocks: ReadonlyArray<Block>, title: string }) : JSX.Element {
-    const totalWidth = props.blocks.reduce((d, i) => d + i.width, 0);
-    const runningIntervals = props.blocks.map(b => createBlock(b.width / totalWidth, b));
+    const totalWidth = props.blocks.reduce((w, b) => w + b.width, 0);
+
+    const minProportionRef = React.useRef(1.0);
+    minProportionRef.current = props.blocks.reduce(
+        (mp, b) => Math.min(b.width / totalWidth, mp),
+        1.0,
+    );
+
+    const innerRef = React.useRef<HTMLDivElement>(null);
+    const seqRef = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+        if (innerRef.current !== null && seqRef.current !== null) {
+            mayExtendSequence(innerRef.current, seqRef.current, minProportionRef.current);
+        }
+    });
+
+    React.useEffect(() => {
+        const observer = new window.ResizeObserver(() => {
+            mayExtendSequence(innerRef.current!, seqRef.current!, minProportionRef.current);
+        });
+        observer.observe(innerRef.current!);
+        return () => { observer.disconnect() };
+    }, []);
+
+    const runningBlocks = props.blocks.map(
+        b => (<RunningBlock proportion={b.width / totalWidth} block={b}/>)
+    );
     return (<div className={styles.Bar}>
         <div className={styles.BarTitle}>{props.title}</div>
-        <div className={styles.BarInner}>{runningIntervals}</div>
+        <div ref={innerRef} className={styles.BarInner}>
+            <div ref={seqRef} className={styles.BarBlockSequence}>{runningBlocks}</div>
+        </div>
     </div>);
 }
 
