8c15a464da21f364b84c43f46ee5c87026e5695a
Eddy Cizeron <eddy.cizeron@shift-technology.com>
2023-07-04 23:46:07 +0200

    Simplify UriQueryConnector

--- app/tools/UriQueryConnector.ts
+++ app/tools/UriQueryConnector.ts
@@ -2,16 +2,14 @@
 
 export class UriQueryConnector {
     private readonly _windowGenerator: () => Window | null | undefined;
-    private _window: Window | null | undefined;
     constructor(windowGenerator: () => Window | null | undefined) {
         this._windowGenerator = windowGenerator;
-        this._window = null;
     }
     
     public injectInUri(...parameters: [string, string | null | undefined][]): void {
-        this._window ??= this._windowGenerator();
-        if (this._window && this._window.location) {
-            const searchParams = new URLSearchParams(this._window.location.search);
+        const window = this._windowGenerator();
+        if (window && window.location) {
+            const searchParams = new URLSearchParams(window.location.search);
             for (const [name, value] of parameters) {
                 if (!value) {
                     searchParams.delete(name);
@@ -19,18 +17,18 @@ export class UriQueryConnector {
                     searchParams.set(name, encodeURIComponent(value));
                 }
             }
-            let path = this._window.location.origin;
+            let path = window.location.origin;
             if (0 < searchParams.size) {
                 path += `?${searchParams.toString()}`;
             }
-            this._window.history.pushState({ path }, '', path);
+            window.history.pushState({ path }, '', path);
         }
     }
 
     public extractFromUri(...setters: [string, (value: string | undefined) => void][]): void {
-        this._window ??= this._windowGenerator();
-        if (this._window && this._window.location) {
-            const searchParams = new URLSearchParams(this._window.location.search);
+        const window = this._windowGenerator();
+        if (window && window.location) {
+            const searchParams = new URLSearchParams(window.location.search);
             for (const [name, setter] of setters) {
                 const value = searchParams.get(name);
                 if (value === null) {
