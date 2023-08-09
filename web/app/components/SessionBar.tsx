ce170a61a39435049d7e7220a19402d5268db385
Eddy Cizeron <eddy.cizeron@shift-technology.com>
2023-08-09 20:43:19 +0200

    Improve SessionBar

--- web/app/components/SessionBar.tsx
+++ web/app/components/SessionBar.tsx
@@ -12,13 +12,28 @@ const OPTIONS: Intl.DateTimeFormatOptions = {
 const DATE_TIME_FORMAT = new Intl.DateTimeFormat('fr-Fr', OPTIONS);
 
 export function SessionBar(props: { session: Session }) : JSX.Element {
+
+    const tags = props.session.tags.map(
+        t => {
+            return (<><span className={styles.Tag}>&nbsp;{t}&nbsp;</span> </>);
+        }
+    );
+
+    const tagLine = 0 < tags.length ?
+        (<tr><td>Tags</td><td>{tags}</td></tr>) :
+        (<></>);
+
+    const commentLine = props.session.comment !== "" ?
+        (<tr><td>Commentaire</td><td>{props.session.comment}</td></tr>) :
+        (<></>);
+
     return (<div className={styles.Bar} key={props.session.id}>
         <table>
-            <tr><td>Programe</td><td>{props.session.formula}</td></tr>
+            <tr><td width={"20%"}>Programe</td><td>{props.session.formula}</td></tr>
             <tr><td>Heure</td><td>{DATE_TIME_FORMAT.format(props.session.time)}</td></tr>
             <tr><td>Lieu</td><td>{props.session.place}</td></tr>
-            <tr><td>Tags</td><td>{props.session.tags.join(', ')}</td></tr>
-            <tr><td>Commentaire</td><td>{props.session.comment}</td></tr>
+            {tagLine}
+            {commentLine}
         </table>
     </div>);
 }
