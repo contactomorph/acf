bef4130162a54b50483a0f016e9fd9be26734613
Eddy Cizeron <eddy.cizeron@shift-technology.com>
2023-12-29 00:36:39 +0100

    Merge home and main files

--- web/app/main.tsx
+++ web/app/main.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './home.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import icon from './assets/icon.svg';
+import './index.css';
+import Model from './model/Model';
+import FirebaseHistoryRepository from './backend/FirebaseHistoryRepository';
+import { Router, RouterKedge } from './routing/Router.tsx';
+import TrainingHistoryPage from './TrainingHistoryPage.tsx';
+import TrainingCreationPage from './TrainingCreationPage.tsx';
+
+/* eslint-disable react-refresh/only-export-components */
+const MODEL = new Model(new FirebaseHistoryRepository());
+const KEDGE = new RouterKedge();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <header>
-      <img className='header-content' src='./app/assets/icon.svg' />
+      <img className='header-content' src={icon} />
       <div className='header-content'>Allures Athlétic Cœur de fond</div>
     </header>
     <main>
-      <Home />
+      <Router kedge={KEDGE}>
+        {{ ctor: cl => (<TrainingHistoryPage client={cl} model={MODEL} />), route: 'history' }}
+        {{ ctor: (cl, v) => (<TrainingCreationPage client={cl} visible={v} />), route: 'creation' }}
+      </Router>
     </main>
   </React.StrictMode>,
 )
