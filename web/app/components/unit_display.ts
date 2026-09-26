29007c9cc8d164fa5e336705a1671642725cec5f
Eddy Cizeron <eddy.cizeron@gmail.com>
2026-09-26 12:06:04 +0200

    Simplify unit display

--- web/app/components/unit_display.ts
+++ web/app/components/unit_display.ts
@@ -12,23 +12,17 @@ export function stringifyDistance(distance: Distance): string {
     const m = Math.floor(distance.in_meter % 1000);
     if (km === 0)
         return `${m}${NBS}m`;
-    if (m === 0)
-        return `${km}${NBS}km`;
-    return `${km}${NBS}km${NBS}${m}${NBS}m`;
+    return `${km}${NBS}${m}${NBS}m`;
 }
 
 export function stringifyTimeSpan(ts: TimeSpan): string {
-    let text = ts.hr !== 0 ? `${ts.hr}${NBS}h${NBS}` : "";
-    const haveSeconds = ts.sec !== 0;
-    if (ts.min !== 0) {
-        text += `${ts.min}${NBS}min${NBS}`;
-    } else if(ts.hr !== 0 && haveSeconds) {
-        text += `0${NBS}min${NBS}`;
+    const secPrefix = ts.sec < 10 ? "0" : "";
+    if(ts.hr !== 0) {
+        const minPrefix = ts.min < 10 ? "0" : "";
+        return `${ts.hr}h${minPrefix}${ts.min}′${secPrefix}${ts.sec}″`;
+    } else {
+        return `${ts.min}′${secPrefix}${ts.sec}″`;
     }
-    if (haveSeconds) {
-        text += `${ts.sec}${NBS}s`;
-    }
-    return text;
 }
 
 export function stringifySpeed(s: Speed): string {
@@ -39,7 +33,7 @@ export function stringifySpeed(s: Speed): string {
 }
 
 export function stringifyPace(p: Pace): string {
-    return `${stringifyTimeSpan(p.in_time_per_km)}${NBS}/${NBS}km`;
+    return stringifyTimeSpan(p.in_time_per_km);
 }
 
 const COLOR_SCALE = chroma.scale(['lightgreen', 'gold', 'orange', 'red', 'darkred']).domain([62, 77, 85, 97, 110]);
